5cad0ab1ddc4f9f82cecc8ac25f55668af9f1a0a
loid345 <volanar@tut.by>
2026-01-20 17:30:51 +0300

    Add bounded event bus queues

--- src/server/app/app_config.py
+++ src/server/app/app_config.py
@@ -58,6 +58,7 @@ class AppConfig(BaseModel):
             event_bus=EventBus.Config(
                 consumers=10,
                 producers=10,
+                queue_max_size=1000,
                 system=EventBus.Config.MwseTcp(
                     type='mwse_tcp',
                     mwse_tcp=MwseTcpEventBusBackend.Config(
